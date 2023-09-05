33dc71274e32d028e4a7764e8121b708ad9fb12c
Beladerm (Artyom) <90094274+Beladerm@users.noreply.github.com>
2023-09-05 20:28:43 +0300

    Update Config.kt

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
  class Config {
      companion object {
-         val token = "OTUzMzk2NjA5ODc2NjU2MTU4.GBf5Lx.cvtyfBL7DwcrmuQ4OWNbQlR6x4_nRZZ-DzmunQ"
+         val token = "YOUR_TOKEN"
      }
  }
